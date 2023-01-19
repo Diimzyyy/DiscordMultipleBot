097b54dbd26c3cf250f7450cad8291c81d1e242a
Diimzyy <40553463+Diimzyyy@users.noreply.github.com>
2023-01-19 15:18:22 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,13 +2,13 @@ import requests, time, json, random
 from colorama import Fore
 
 # Isi channel ID 
-channel = ['1065535137145688104','1065535155458027540']
+channel_ids = ['Channel1','Channel2']
 
 # Tulis Kata Kata Mutiaramu
-text = ['test1','test2','test3']
+text = ['text1','text2','text3']
 
 # Isi Token
-token = ['NDI1MjY4MzE3NTI0MDY2MzA1.GNBDwf.P-jcjyQ0gNrfA0bp5-HOONYb3Hsl5qfOkIzC58','OTA5MDE2MjQ5NjQyNjgwMzUw.GUKYcc.W3znAcPnfo8P6CT7P43bnlJAke0_VPIWeYehS0']
+token = ['TOKEN_DISCORD1','TOKEN_DISCORD2']
 
 while True:
 
@@ -16,7 +16,7 @@ while True:
     channel_id = random.choice(channel_ids)
 
     payload = {
-        'content': random.choice(words)
+        'content': random.choice(text)
     }
 
 
@@ -34,11 +34,10 @@ while True:
     if response.status_code == 200:
         messages = response.json()
         if len(messages) == 0:
-            # Tidak ada pesan lagi di channel, berhenti menghapus pesan
-            break
+            
         else:
-            # Tambahkan jeda selama 20 detik sebelum menghapus pesan
-            time.sleep(0) 
+            # Jeda 2 Detik Sebelum Menghapus Pesan
+            time.sleep(2) 
 
             # Hapus pesan terbaru di channel
             message_id = messages[0]['id']
@@ -50,7 +49,7 @@ while True:
     else:
         print(f'Gagal mendapatkan pesan di channel: {response.status_code}')
 
-            # Angka 30 adalah countdown untuk next text sesuaikan dengan keinginan kalian
-    for i in range(30): 
-        print(Fore.WHITE + f"Sabar ya ganteng {30-i}", end='\r')
+            # Angka 3 adalah countdown untuk next text sesuaikan dengan keinginan kalian
+    for i in range(3): 
+        print(Fore.WHITE + f"Menunggu {3-i} Detik", end='\r')
         time.sleep(1) 
